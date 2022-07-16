d9e0d06e61315a5a5891f313eef32650055691e2
vishnu5104 <knp19cs044@ceknpy.ac.in>
2022-07-16 11:41:50 +0530

    new add

---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+
+=======
+# OWN IT
+>>>>>>> 0eec22d9e59a07bca32847f1aba5f0e9cb3953fd
---
+++ context/constants.js
@@ -0,0 +1,4 @@
+import marketAbi from './NFTMarketplace.json';
+
+export const MarketAddress = '0x173714E8bA7E80399656Cc9B4B7741cb0Ddf83f4';
+export const MarketAddressABI = marketAbi.abi;
